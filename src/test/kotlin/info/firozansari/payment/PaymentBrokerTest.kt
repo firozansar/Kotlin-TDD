c1a639d4c5b1f95e839419250939aac58c5f01fa
Firoz Ansari <firozansar@gmail.com>
2022-04-09 19:39:23 +0100

    Fix PaymentBrokerTest

--- src/test/kotlin/info/firozansari/payment/PaymentBrokerTest.kt
+++ src/test/kotlin/info/firozansari/payment/PaymentBrokerTest.kt
@@ -1,26 +1,27 @@
 package info.firozansari.payment
 
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MockKExtension::class)
 class PaymentBrokerTest {
 
-    @MockK
+    @RelaxedMockK
     private lateinit var wallet: WalletInterface
 
-    @MockK
+    @RelaxedMockK
     private lateinit var provider: PaymentProviderInterface
 
-    @MockK
+    @InjectMockKs
     private lateinit var broker: PaymentBroker
 
     @Test
-    @Throws(InsufficientFundsException::class, ProviderNotAvailableException::class)
     fun testPay_WalletHasFundsAndProviderIsAvailableAndDepositSucceeded_ShouldReturnTrue() {
         // Arrange
         val amount = 10
@@ -36,33 +37,37 @@ class PaymentBrokerTest {
 
     // Assert
     @Test
-    @Throws(InsufficientFundsException::class, ProviderNotAvailableException::class)
+    @Throws(InsufficientFundsException::class)
     fun testPay_WalletDoesNotHaveFunds_ShouldThrowInsufficientFundsException() {
-        // Arrange
-        val amount = 10
-        val balance = 9
+        assertThrows<InsufficientFundsException> {
+            // Arrange
+            val amount = 10
+            val balance = 9
 
-        every { wallet.balance } returns balance
-        every { provider.isAvailable } returns true
-        every { provider.deposit(wallet.id, amount) } returns true
+            every { wallet.balance } returns balance
+            every { provider.isAvailable } returns true
+            every { provider.deposit(wallet.id, amount) } returns true
 
-        // Act
-        broker.pay(amount)
+            // Act
+            broker.pay(amount)
+        }
     }
 
     // Assert
     @Test
-    @Throws(InsufficientFundsException::class, ProviderNotAvailableException::class)
+    @Throws(ProviderNotAvailableException::class)
     fun testPay_ProviderIsNotAvailable_ShouldThrowProviderNotAvailableException() {
-        // Arrange
-        val amount = 10
-        val balance = 20
+        assertThrows<ProviderNotAvailableException> {
+            // Arrange
+            val amount = 10
+            val balance = 20
 
-        every { wallet.balance } returns balance
-        every { provider.isAvailable } returns false
-        every { provider.deposit(wallet.id, amount) } returns true
+            every { wallet.balance } returns balance
+            every { provider.isAvailable } returns false
+            every { provider.deposit(wallet.id, amount) } returns true
 
-        // Act
-        broker.pay(amount)
+            // Act
+            broker.pay(amount)
+        }
     }
 }
