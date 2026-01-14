f24ae859d709d656b7c5007f0a754f30372b4abe
Firoz Ansari <firozansar@gmail.com>
2026-01-14 06:54:02 +0000

    Update Junit and Mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ dependencies {
     implementation("com.github.ajalt.mordant:mordant:2.4.0")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.2")
+    testImplementation("io.mockk:mockk:1.14.7")
 }
 tasks.test {
     useJUnitPlatform()
